169b3431a611062c648ae499d232c67020beb39f
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-03-15 10:16:19 +1100

    Added xml extraction to run script.

--- run_call.py
+++ run_call.py
@@ -1,10 +1,10 @@
 from api_call import *
+from extract_xml import *
+from xml_load_and_process import *
+
 """
 Local variables
 """
-
-debug_log_config("api_call")
-logger = logging.getLogger()
 
 ## Loads environment variables
 # One MMS id as a string
@@ -25,8 +25,26 @@ Generates the list of ids from the original string.
 Generates the dictionary of chunks from the list.
 
 Checks if the API key is valid, then iterates through the chunks.
-Each API response is parsed from josn, then written to a file in a subfolder titled json.
+Each API response is parsed from josn, then written to a file 
+in a subfolder titled json.
 """
+
+print(
+    "This process is currently configured to call the API based on " 
+    + "a list of MMS Ids, transform them into XML, and return a "
+    + "list of parent MMS Ids found in the records."
+)
+print("Do you wish to continue? (y/n)")
+user_input = input()
+
+if user_input.lower().startswith("y"):
+    print("Running program...")
+else:
+    print("Exiting program")
+    sys.exit()
+
+# api_call
+
 list_ids = split_identifiers(MMS_IDS)
 chunked_calls = chunk_identifiers(list_ids)
 
@@ -35,3 +53,28 @@ if check_api_key():
         response = get_bibs(key, chunked_calls[key])
         parsed_json = get_json_string(response)
         output_bib_files("json", key, parsed_json)
+
+
+# extract_xml
+
+source_directory = path.join("json")
+output_directory = path.join("output", "xml")
+
+iterate_directory(source_directory, output_directory)
+
+# xml_load_and_process
+
+
+"""
+Still under development. Requires a write function to
+output list of ids to useful list.
+"""
+
+#xml_path = os.path.join("output", "xml")
+#output_path = os.path.join("output", "parent_id")
+# List for storing MMS Ids retrieved from 950$p
+#parent_ids = []
+
+#files = get_callable_files(xml_path)
+#parent_ids = iterate_get_parents(files)
+
