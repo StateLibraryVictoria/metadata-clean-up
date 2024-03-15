3b314dfef27352b2a5f542484ee6868191b97d40
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-03-15 14:40:12 +1100

    Improved logging information.

--- extract_xml.py
+++ extract_xml.py
@@ -16,7 +16,7 @@ Generates a list of records as dictionary "mms_id" : "xml" key value pairs.
 
 def get_record(json_file):
     bibs = json.loads(json_file)
-    logger.debug("json loaded")
+    logger.debug("Json loaded from api output")
     records = {}
     for item in bibs["bib"]:
         for key in item:
@@ -45,6 +45,7 @@ Output: Record to file in ./output/xml
 
 
 def write_records(dictionary, output_dir):
+    log_list = []
     for key in dictionary:
         file = open(
             path.join(output_dir, f"record_{key}.xml"),
@@ -53,8 +54,10 @@ def write_records(dictionary, output_dir):
             errors="backslashreplace",
         )
         file.write(dictionary[key][0])
-        logger.debug(f"Created file for record: {key}")
+        log_list += [key]
         file.close()
+    final_list = ";".join(log_list)
+    logger.debug(f"Created file for records: {final_list}")
 
 
 """
@@ -78,9 +81,9 @@ def iterate_directory(dir_name, output_dir):
                 records = get_record(data)
                 fixed_header = fix_header_encoding(records)
                 write_records(fixed_header, output_dir)
-                logger.info("Wrote records to: " + output_dir)
+                logger.info("Records written to: " + output_dir)
             except Exception as e:
-                logger.error(f"Error occured: {e}")
+                logger.error(f"Error occured while iterating dictionary: {e}")
                 break
         return True
 
@@ -98,16 +101,3 @@ def fix_header_encoding(dictionary):
         string = string.replace('<?xml version="1.0" encoding="UTF-16"?>', "")
         dictionary[key][0] = string
     return dictionary
-
-
-## Test file
-# FILE = getenv("JSON_RECORD")
-# json_request = open(FILE), "r", encoding="utf-8", errors="backslashreplace")
-# json_loaded = json_request.read()
-
-# Main call:
-source_directory = path.join("json")
-logger.debug(f"Source: {source_directory}")
-output_directory = path.join("output", "xml")
-logger.debug(f"output: {output_directory}")
-iterate_directory(source_directory, output_directory)
