3348a9c1b2273661594e7f353e87145de535efef
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-03-22 14:04:37 +1100

    Added handling for trailing period on $2 gmgpc.

--- xml_load_and_process.py
+++ xml_load_and_process.py
@@ -166,9 +166,11 @@ def fix_655_gmgpc(record):
     fields = record.get_fields('655')
 
     for field in fields:
+        field['2'] = 'gmgpc' if field['2'].startswith('gmgpc') \
+            else field['2'] # strips out trailing punctuation/whitespace in $2
         value = field['a'] if field['2'] == 'gmgpc' else ""
 
-        if value.endswith("."):
+        if value.endswith("."): # only updates gmgpc with final period
             field['a'] = value[0:-1]
     
     return record
