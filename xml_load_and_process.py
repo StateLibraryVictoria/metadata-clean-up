eb1ca0ee842f5c6757e7979ccfcc73e6d4fb79a4
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-03-28 10:32:12 +1100

    Added handling for different 1xx fields on replace.

--- xml_load_and_process.py
+++ xml_load_and_process.py
@@ -58,10 +58,6 @@ def get_callable_files(dir_name):
     except Exception as e:
         logger.error(f"Error getting callable files: {e}")
 
-"""
-Input: pymarc record object.
-Output: Count of matched fields.
-"""
 
 def get_field_count(record, field):
     """Counts number of fields in record
@@ -95,7 +91,9 @@ def get_field_from_source(source_record, field):
         subfields = field.subfields
 
     # Check that field in source record.
-    if get_field_count(source_record, tag) != 1:
+    if tag.startswith("6"):
+        return source_record.get_fields(tag)
+    elif get_field_count(source_record, tag) != 1:
         amount = f"no {tag} fields" if get_field_count(source_record, tag) < 1 else f"too many {tag} fields"
         logger.warning(f"Source record contains {amount}. "
                         + "Cancelling operation.")
@@ -122,11 +120,25 @@ def add_field_to_target(target_record, field, replace=True):
     subfields = field.subfields
         
     if replace:
+        logger.info("Processing record: " + target_record.get_fields('001')[0].value())
         try: # try to remove the fields from the record to be updated
-            logger.info(f"Removing {len(target_record.get_fields(tag))} subfields from record.")
-            print(f"Removing {len(target_record.get_fields(tag))} subfields from record.")
-            for f in target_record.get_fields(tag):
-                target_record.remove_field(f)
+            if tag.startswith("1"):
+                source_1xx = target_record.get_fields('100', '110', '111')
+                logger.info(f"Removing {len(source_1xx)} subfields from record.")
+                print(f"Removing {len(source_1xx)} subfields from record.")
+                if len(source_1xx) > 1:
+                    print("Multiple 1xx fields identified. See log for details.")
+                    logger.warn("Multiple 1xx fields identified. All fields have been removed.")
+                for f in source_1xx:
+                    logger.info(f"Record contains field: {f.tag}")
+                    print(f"Record contains field: {f.tag}")
+                    target_record.remove_field(f)
+                    logger.info(f"Success: " + f.tag + " " + f.format_field() + " removed from record.")
+            else:
+                logger.info(f"Removing {len(target_record.get_fields(tag))} subfields from record.")
+                print(f"Removing {len(target_record.get_fields(tag))} subfields from record.")
+                for f in target_record.get_fields(tag):
+                    target_record.remove_field(f)
             logger.info(f"Success: {tag} removed from target.")
             print(f"Success: {tag} removed from target.")
         except Exception as e:
@@ -167,11 +179,34 @@ def fix_655_gmgpc(record):
     fields = record.get_fields('655')
 
     for field in fields:
-        field['2'] = 'gmgpc' if field['2'].startswith('gmgpc') \
-            else field['2'] # strips out trailing punctuation/whitespace in $2
-        value = field['a'] if field['2'] == 'gmgpc' else ""
+        if len(field.get_subfields('2')) > 0:
+            field['2'] = 'gmgpc' if field['2'].startswith('gmgpc') \
+                else field['2'] # strips out trailing punctuation/whitespace in $2
+            value = field['a'] if field['2'] == 'gmgpc' else ""
 
-        if value.endswith("."): # only updates gmgpc with final period
-            field['a'] = value[0:-1]
+            if value.endswith("."): # only updates gmgpc with final period
+                field['a'] = value[0:-1]
     
     return record
+
+def is_parent(record):
+    fields = record.get_fields('956')
+    for field in fields:
+        if field['b'] == "PARENT":
+            return True
+    return False
+
+def record_to_mrc(record, output_filename):
+    """Write xml record to mrc file.
+    
+    Args:
+        record (pymarc Record object)
+        output_filename (str): path and filename for output mrc file.
+
+    Processing:
+        XML is translated into MARC binary and appended to desired file.
+    """
+    with open(output_filename, 'ab') as out:
+        out.write(record.as_marc())
+    out.close()
+
