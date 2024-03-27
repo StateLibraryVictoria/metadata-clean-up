0a7474958f946a0353db2fe16a7738f7c1a72633
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-03-28 10:35:25 +1100

    Added documentation strings to methods.

--- xml_load_and_process.py
+++ xml_load_and_process.py
@@ -7,14 +7,15 @@ from logger_config import *
 debug_log_config("load-xml")
 logger = logging.getLogger()
 
-"""
-Input: Pymarc record object and desired field and subfield.
-Processing: Checks if the field exists
-Output: returns value stored in specified tag.
-"""
-
 
 def get_marc_tag(pymarc_record, field, subfield):
+    """Retrieve value in specified field.
+
+    Args:
+        pymarc_record: Pymarc Record Object.
+        field: str - MARC tag, eg '001', '500', etc.
+        subfield: str - Subfield tag eg. 'a', '4'.
+    """
     try:
         value = pymarc_record[field][subfield]
         return value
@@ -24,14 +25,14 @@ def get_marc_tag(pymarc_record, field, subfield):
                      + " Mms id: " + pymarc_record['001'].value())
 
 
-"""
-Input: Filepath to single xml record.
-Processing: passes to pymarc
-Output: First item in the array (aka the record)
-"""
 
 
 def load_pymarc_record(filename):
+    """Load XML record as pymarc record object.
+    
+    If 1 record in the array, returns record object. 
+    Otherwise prints warning and returns record array.
+    """
     record = pymarc.parse_xml_to_array(filename)
     if len(record) == 1:
         return record[0]
@@ -40,14 +41,10 @@ def load_pymarc_record(filename):
         return record
 
 
-"""
-Input: Directory where target files are held
-Processing: Creates filpath using directory and name
-Output: List of filepaths
-"""
 
 
 def get_callable_files(dir_name):
+    """Get a list of filepaths in target directory"""
     output_list = []
     try:
         for root, dirs, files in os.walk(dir_name):
