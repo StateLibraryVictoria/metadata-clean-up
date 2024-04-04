238fdcc9b678d1f25ded9b86fa25bac5c9a489ec
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-04-04 11:07:25 +1100

    Moved conftest.py to tests/

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 import shutil
 import os
-from shared_functions import *
+from src.shared_functions import *
 
 # Set the project root to resolve source file.
 ROOT_DIR = os.path.abspath(os.curdir)
@@ -9,6 +9,7 @@ ROOT_DIR = os.path.abspath(os.curdir)
 # Path variables
 filename = "test_file_with_errors.mrc"
 test_marc_file = os.path.join(ROOT_DIR, "tests","test_data","marc_data","test_file_with_errors.mrc")
+test_parents = os.path.join(ROOT_DIR, "tests","test_data","marc_data", "parent")
 missing_records = ["9938846603607636"]
 
 # Create MARC file
@@ -26,13 +27,17 @@ def setup_working_directory(tmp_path_factory):
     setup_directories()
     yield location
 
-# Get missing parents for marc file
+# Create copy of required match records in output directory.
 @pytest.fixture(scope="session")
 def missing_parents(tmp_path_factory):
     location = tmp_path_factory.getbasetemp()
-    os.chdir(location)
-    identifiers = split_marc_records(test_marc_file)
     output_dir = os.path.join("output", "mrc", "split", "parent")
-    get_missing_records(identifiers["parent_records"], identifiers["parent_ids"], output_dir)
+    output = os.path.join(location, output_dir)
+    os.chdir(location)
+    try:
+        copydir = output
+        shutil.copytree(test_parents, copydir, dirs_exist_ok=True)
+    except Exception as e:
+        print(f"Error occured copying parents in test: {e}")   
     output = os.path.join(location, output_dir)
     yield output
