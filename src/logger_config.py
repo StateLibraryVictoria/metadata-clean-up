04f054b12ea054e22374cc8c2074b24972cb33cc
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-04-08 15:58:31 +1000

    Added name of script to format.

--- src/logger_config.py
+++ src/logger_config.py
@@ -3,4 +3,4 @@ from os import path
 
 def debug_log_config(name):
     path_name = path.join("logs", "".join(('log_',name,'.log')))
-    logging.basicConfig(filename=path_name, encoding='utf-8', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename=path_name, encoding='utf-8', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
