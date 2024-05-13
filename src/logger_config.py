1af0f361dd12283ee129167032e44f4bb60f713e
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-05-13 12:06:49 +1000

    Removed redundent file.

--- src/logger_config.py
+++
@@ -1,6 +0,0 @@
-import logging
-from os import path
-
-def debug_log_config(name):
-    path_name = path.join("logs", "".join(('log_',name,'.log')))
-    logging.basicConfig(filename=path_name, encoding='utf-8', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
