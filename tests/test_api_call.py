21e5ebe34db804d98b60562921fb92518d873b99
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-05-21 09:12:39 +1000

    Passed key to function.

--- tests/test_api_call.py
+++ tests/test_api_call.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime
 import pytest
 
@@ -9,38 +8,71 @@ from src.api_call import *
 
 
 # Checks that lists of mms_ids are split correctly. Includes invalid MMS Ids that should be removed.
-@pytest.mark.parametrize("mms_ids, split, count", 
-                         [("9938164143607636", ["9938164143607636"], 1), ("9938036653607636,9938036613607636,9938164143607636", ["9938036653607636","9938036613607636","9938164143607636"], 3), ("9938036653607636,997636,not an mms id", ["9938036653607636"], 1)])
+@pytest.mark.parametrize(
+    "mms_ids, split, count",
+    [
+        ("9938164143607636", ["9938164143607636"], 1),
+        (
+            "9938036653607636,9938036613607636,9938164143607636",
+            ["9938036653607636", "9938036613607636", "9938164143607636"],
+            3,
+        ),
+        ("9938036653607636,997636,not an mms id", ["9938036653607636"], 1),
+    ],
+)
 def test_split_identifiers(mms_ids, split, count):
     list_ids = split_identifiers(mms_ids)
     assert list_ids == split
     assert len(list_ids) == count
 
+
 # This would be better if it could check an example with more than 100 ids. Currently just checks a few to make sure the formatting comes out right.
-@pytest.mark.parametrize("mms_ids, chunked", [(["9938164143607636"], {'0': "9938164143607636"} ), (["9938036653607636","9938036613607636","9938164143607636"], {'0': "9938036653607636,9938036613607636,9938164143607636"})])
+@pytest.mark.parametrize(
+    "mms_ids, chunked",
+    [
+        (["9938164143607636"], {"0": "9938164143607636"}),
+        (
+            ["9938036653607636", "9938036613607636", "9938164143607636"],
+            {"0": "9938036653607636,9938036613607636,9938164143607636"},
+        ),
+    ],
+)
 def test_chunk_identifiers(mms_ids, chunked):
     chunked_identifiers = chunk_identifiers(mms_ids)
     assert chunked_identifiers == chunked
 
+
 # Checks if the MMS Id validator is behaving as expected. Can only test for obvious issues such as not long enough and not starting and ending with the right characters.""
-@pytest.mark.parametrize("id, expected", [("9938164143607636", True), ("997636", False), ("not an mms id", False), ("99alsonot", False), ("999912376367636", False), ("999912347636", True)])
+@pytest.mark.parametrize(
+    "id, expected",
+    [
+        ("9938164143607636", True),
+        ("997636", False),
+        ("not an mms id", False),
+        ("99alsonot", False),
+        ("999912376367636", False),
+        ("999912347636", True),
+    ],
+)
 def test_validate_mmsid(id, expected):
     assert validate_mmsid(id) == expected
 
+
 # Checks that the API key is configured correctly. This does call the API.
 def test_check_api_key():
-    assert check_api_key() == True
+    KEY = os.getenv("KEY")
+    assert check_api_key(KEY) == True
+
 
 # Checks that the json files are created as expected.
 def test_output_json_files(tmp_path):
     location = tmp_path / "json"
     location.mkdir()
-    part = '0'
+    part = "0"
     input = "some text"
-    today = datetime.now().strftime('%Y%m%d%H%M00')
+    today = datetime.now().strftime("%Y%m%d%H%M00")
     output_json_files(location, part, input)
     filename = f"{today}_records_batch_{part}.json"
     file = location / filename
     assert file.read_text() == "some text"
     assert len(list(tmp_path.iterdir())) == 1
-
