e68d81291ef5d4d1ff292e1c3397625a7e01cf8f
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-05-22 15:51:03 +1000

    Tidied up functions and removed unncessary code.

--- src/api_call.py
+++ src/api_call.py
@@ -1,9 +1,11 @@
 import logging
-import json
 import requests
 import os
 import sys
-from datetime import datetime
+
+logger = logging.getLogger()
+
+API_CALL_LIMIT = 10000
 
 
 def api_volume_check(api_return):
@@ -83,6 +85,9 @@ def chunk_identifiers(id_list):
     return request_dict
 
 
+BASEURL = "https://api-ap.hosted.exlibrisgroup.com/almaws/v1/bibs/"
+
+
 def check_api_key(key):
     """Makes a test call to the API and returns a boolean."""
     if key == None:
@@ -119,55 +124,3 @@ def get_bibs(part, mms_ids, key):
     logger.debug(f"API GET request sent. Batch number {part}")
     logger.debug(api_call)
     return api_call
-
-
-def get_json_string(api_call):
-    """Gets converts request to JSON"""
-    data = api_call.json()
-    json_str = json.dumps(data, indent=4)
-    return json_str
-
-
-def output_json_files(dir, part, input):
-    """Writes returned json string to files.
-    Args:
-        dir (path) - output location.
-        part (str | int) - added to filename to distinguish from other filenames.
-        input (str) - json string.
-
-    Output:
-        file with name format {YYYYMMDDHM00}_records_batch_{part}.json
-    """
-    today = datetime.now().strftime("%Y%m%d%H%M00")
-    filename = os.path.join(dir, f"{today}_records_batch_{part}.json")
-    try:
-        with open(filename, "w", encoding="utf-8", errors="backslashreplace") as file:
-            file.write(input)
-        logger.debug(f"Output API response to file {filename}")
-    except Exception as e:
-        logger.debug(f"Error occured: {e}")
-
-
-""" 
-Configure logfile. 
-
-Creates a log file with the name log_api_call.log that will capture actions that happen
-during the API call.
-Every time the script is run it will append log to the same file.
-
-Logger calls are used throughout the program and will record hidden info including the http request.
-"""
-logger = logging.getLogger()
-
-
-## Loads environment variables
-# One MMS id as a string
-MMS_ID = os.getenv("MMS_ID")
-## Must be a list of ids separated by commas.
-MMS_IDS = os.getenv("MMS_IDS")
-# Alma API key
-logger.debug("Loaded environment variables")
-
-# Local variables
-API_CALL_LIMIT = 10000
-BASEURL = "https://api-ap.hosted.exlibrisgroup.com/almaws/v1/bibs/"
